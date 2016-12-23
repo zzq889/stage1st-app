a29591235ab5712a3e056f9800b7355be2323bed
Zeqing Zhang <zzq889@gmail.com>
2016-12-23 13:15:23 +0800

    add session back

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux-immutable';
 import { NavigationReducer } from '@exponent/ex-navigation';
+import SessionStateReducer from '../modules/session/SessionState';
 import ErrorStateReducer from '../modules/error/ErrorState';
 import EntitiesStateReducer from '../modules/entities/EntitiesState';
 import PaginationStateReducer from '../modules/pagination/PaginationState';
@@ -8,6 +9,7 @@ export default combineReducers({
   // @NOTE: By convention, the navigation state must live in a subtree called
   // `navigation`
   navigation: NavigationReducer,
+  session: SessionStateReducer,
   errorMessage: ErrorStateReducer,
   entities: EntitiesStateReducer,
   pagination: PaginationStateReducer,
