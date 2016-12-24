73cbdaac5538194fe99353e01b8f241244b29803
Zeqing Zhang <zzq889@gmail.com>
2016-12-25 01:41:34 +0800

    change store persistence to whitelist

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -6,14 +6,18 @@ import EntitiesStateReducer from '../modules/entities/EntitiesState';
 import PaginationStateReducer from '../modules/pagination/PaginationState';
 import AuthStateReducer from '../modules/auth/AuthState';
 
+// after reducer changes, don't forget to update the persist policy in store.js
 export default combineReducers({
   // @NOTE: By convention, the navigation state must live in a subtree called
   // `navigation`
   navigation: NavigationReducer,
   session: SessionStateReducer,
   error: ErrorStateReducer,
+
+  // persist
   entities: EntitiesStateReducer,
   pagination: PaginationStateReducer,
   auth: AuthStateReducer,
+
   // others
 });
--- src/redux/store.js
+++ src/redux/store.js
@@ -43,7 +43,7 @@ sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store, {
   storage: AsyncStorage,
-  blacklist: ['error', 'navigation'],
+  whitelist: ['entities', 'pagination', 'auth'],
 });
 
 export default store;
