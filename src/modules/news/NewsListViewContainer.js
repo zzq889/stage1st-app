dd9daad02dcde359e853da13054ea783b04e7e39
candy <candy.zhengxq@gmail.com>
2017-03-19 15:55:01 +0800

    暂时设定全局的信息处理模式为浅拷贝 不加缓存策略 (#39)
    * 暂时设定全局的信息处理模式为浅拷贝 不加缓存策略
    
    * 修复store没有初值引发的空指针问题
    
    * start modify articles behavior
    
    * 1.去除多余的console.log
    2. 对news做了排序和filter
    
    * 做一个防空处理

--- src/modules/news/NewsListViewContainer.js
+++ src/modules/news/NewsListViewContainer.js
@@ -5,7 +5,10 @@ import { loadNewsPage } from './NewsState';
 
 export default connect(
   state => ({
-    news: state.getIn(['entities', 'articles'], List()).toList(),
+    news: state
+    .getIn(['entities', 'articles'], List())
+    .sortBy(news => news.get('date'))
+    .reverse(),
   }),
   { loadNewsPage },
 )(NewsListView);
